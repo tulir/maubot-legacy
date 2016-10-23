1d5fff1f2759b192120261f22860ed15b54a5d32
Tulir Asokan <tulir@maunium.net>
2016-10-23 13:42:01 +0300

    Improve things, add stdio handler flag

--- cmd/maubottest/maubottest.go
+++ cmd/maubottest/maubottest.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"maunium.net/go/maubot"
+	"maunium.net/go/maubot/stdio"
 	"maunium.net/go/maubot/telegram"
 	"maunium.net/go/mauflag"
 )
 
-var tgKey = mauflag.MakeKey("t", "telegram").String()
+var tgKey = mauflag.MakeFull("t", "telegram", "The Telegram bot secret to use.", "").String()
+var useStdIO = mauflag.MakeFull("i", "stdio", "Whether or not to enable the stdio handler", "false").Bool()
+var wantHelp, _ = mauflag.MakeHelpFlag()
 
 func main() {
 	mauflag.Parse()
+	mauflag.SetHelpTitles("maubottest - A simple testing utility for maubot", "maubottest [-i] [-t secret]")
+	if *wantHelp {
+		mauflag.PrintHelp()
+		return
+	}
 	maubot := maubot.Create()
 
 	if len(*tgKey) > 0 {
@@ -21,11 +29,13 @@ func main() {
 		maubot.Add(tg)
 		tg.Connect()
 	}
+	if *useStdIO {
+		io := stdio.New()
+		maubot.Add(io)
+		io.Connect()
+	}
 
 	for message := range maubot.Messages {
-		fmt.Printf("<%s> %s\n", message.Room(), message.Text())
-		if len(message.Text()) > 10 {
-			message.Reply("*Yay?!*")
-		}
+		message.Reply(fmt.Sprintf("Hello, %s. You said %s", message.Sender(), message.Text()))
 	}
 }
